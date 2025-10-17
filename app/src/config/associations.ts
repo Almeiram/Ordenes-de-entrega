c3110847c7205f1d39c1c5ebfe0e614281d3252a
Almeiram <mariaalmeira61@gmail.com>
2025-10-17 10:21:07 -0500

    feat: correction in configurations

--- app/src/config/associations.ts
+++ app/src/config/associations.ts
@@ -1,45 +1,10 @@
 import Role from "../models/role.model";
 import Access from "../models/access.model";
-import User from "../models/user.model";
 
 export const applyAssociations = () => {
   // Roles y Accesos
   // Un rol puede tener muchos accesos, pero un acceso solo puede tener un rol
-  Role.hasMany(Access, {
-    foreignKey: "role_id",
-    as: "accesses",
-  });
-  Access.belongsTo(Role, {
-    foreignKey: "role_id",
-    as: "role",
-  });
+  Access.belongsTo(Role, { foreignKey: "role_id", as: "role" });
+  Role.hasMany(Access, { foreignKey: "role_id", as: "users" });
 
-  // Users y Accesos
-  // Un usuario puede tener un acceso y viceversa
-  Access.hasOne(User, {
-    foreignKey: "access_id",
-    as: "user",
-  });
-  User.belongsTo(Access, {
-    foreignKey: "access_id",
-    as: "access",
-  });
-
-  // // Bodega y Productos_bodega
-  // // Una bodega puede tener muchos productos_bodega y un productos_bodega solo puede tener una bodega
-  // Warehouse.belongsToMany(Product, {
-  //   through: ProductWarehouse,
-  //   foreignKey: "warehouse_id", // Clave foránea en ProductBodega que apunta a Bodega
-  //   otherKey: "product_id", // Clave foránea en ProductBodega que apunta a Product
-  //   as: "products", // Alias para la consulta desde Bodega
-  // });
-
-  // // Producto y Productos_bodega
-  // // Un producto puede tener muchos productos_bodega y un productos_bodega solo puede tener un producto
-  // Product.belongsToMany(Warehouse, {
-  //   through: ProductWarehouse,
-  //   foreignKey: "product_id", // Clave foránea en ProductBodega que apunta a Product
-  //   otherKey: "warehouse_id", // Clave foránea en ProductBodega que apunta a Bodega
-  //   as: "warehouses", // Alias para la consulta desde Product
-  // });
 };
--- app/src/config/database.ts
+++ app/src/config/database.ts
@@ -2,9 +2,6 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("Database Host:", process.env.DB_HOST);
-console.log("Database User:", process.env.DB_USER);
-console.log("Database Name:", process.env.DB_NAME);
 
 const sequelize = new Sequelize(
     process.env.DB_NAME as string,
